b0f39671c1749cc55ec968aeca52f266b9603669
Du-Ar <Najiblayap132@gmail.com>
2024-09-21 15:06:07 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author Du.Ar
+ */
+public class LuasSegitiga {
+
+    public static void main(String[] args) {
+        int alas = 7;
+        int tinggi = 3;
+        double luasSegitiga =(double) alas * tinggi / 2;
+
+        System.out.println(luasSegitiga);
+    }
+}
